04bc5db28b72a3b0761fe7f02f7f93c8372a848e
anninmoon <79011090+anninmoon@users.noreply.github.com>
2021-03-04 17:34:40 -0600

    agentedinamicobasico
    Tiene un comportamiento básico de ir y regresar

---
+++ proyecto final/Assets/Scripts/AgenteDinamico.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgenteDinamico : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public Transform[] waypoints;
+
+    public int indexWaypoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(waypoints[indexWaypoint].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance == 0)
+        {
+            indexWaypoint = (indexWaypoint + 1) % waypoints.Length;
+
+            agent.SetDestination(waypoints[indexWaypoint].position);
+        }
+    }
+}
